e61301a78319c3f2927e3b3586ad9a04d38d2df7
Pirate787 <brofesor787@gmail.com>
2026-06-22 22:11:05 +0300

    Add Express server

--- index.js
+++ index.js
@@ -10,3 +10,14 @@ client.once('ready', () => {
 });
 
 client.login(process.env.DISCORD_TOKEN);
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('4o4 Bot is alive!');
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Web server started');
+});
