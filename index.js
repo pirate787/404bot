9ed04a40d289b477c56dbb175ca48ce0803c5d95
Pirate787 <brofesor787@gmail.com>
2026-06-22 22:34:12 +0300

    Add Express server

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 require('dotenv').config();
 const { Client, GatewayIntentBits } = require('discord.js');
+const express = require('express');
 
 const client = new Client({
     intents: [GatewayIntentBits.Guilds]
 });
 
-client.once('ready', () => {
-    console.log(`✅ ${client.user.tag} is online!`);
-});
-
-client.login(process.env.DISCORD_TOKEN);
-
-const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
@@ -21,3 +15,9 @@ app.get('/', (req, res) => {
 app.listen(process.env.PORT || 3000, () => {
     console.log('Web server started');
 });
+
+client.once('ready', () => {
+    console.log(`✅ ${client.user.tag} is online!`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
